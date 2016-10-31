01275cd0955f0b3c11b841245400e2d07bd5f3ad
adybhav <abhavnan@buffalo.edu>
2016-10-30 20:27:47 -0400

    Update widget.cpp

--- GUI/widget.cpp
+++ GUI/widget.cpp
@@ -2,20 +2,15 @@
 #include "qwidget.h"
 #include"QWindow"
 #include "QWidget"
-//#include<QProcess>
 #include <iostream>
-//#include<QString>
-//#include<QStringList>
 #include<QVBoxLayout>
 #include "ui_widget.h"
 
 Widget::Widget(QWidget *parent) :
-    //QString command==
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
-    //QProcess *process = new QProcess(command, parameters);
 
     QWindow *window = QWindow::fromWinId(90177538);
     window->setFlags(Qt::FramelessWindowHint);
